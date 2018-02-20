6d906bb3713ec73da5c95cd3c857c582f970f2e0
Perakis Georgios <perakisgeorgios@gmail.com>
2018-02-20 17:27:04 +0200

    implemented get_df_stratified_split_in_train_validation()

--- AnalyticsEngine/load_data.py
+++ AnalyticsEngine/load_data.py
@@ -2,7 +2,9 @@ from bs4 import BeautifulSoup as Soup
 import pandas as pd
 from bs4 import Tag
 from AnalyticsEngine import DATA_DIR, setup_logger
+from sklearn.model_selection import StratifiedShuffleSplit
 import re
+from collections import Counter
 
 logger = setup_logger(__name__)
 
@@ -63,7 +65,79 @@ def parse_reviews(file='ABSA16_Laptops_Train_SB1_v2.xml',
     return extracted_data
 
 
+def get_df_stratified_split_in_train_validation(data,
+                                                label,
+                                                validation_size,
+                                                random_state=5):
+    """
+
+    :param data:
+    :param label:
+    :param validation_size:
+    :param random_state:
+    :return:
+    """
+
+    data.reset_index(drop=True, inplace=True)
+    X = data.drop(label, axis=1).copy()
+    y = data[label].copy()
+
+    train_validation_sss = StratifiedShuffleSplit(test_size=validation_size,
+                                                  random_state=random_state)
+
+    x_train, x_val, y_train, y_val = None, None, None, None
+
+    for train_index, validation_index in train_validation_sss.split(X, y):
+        x_train, x_val = X.iloc[train_index], X.iloc[validation_index]
+        y_train, y_val = y.iloc[train_index], y.iloc[validation_index]
+
+    proportions = lambda x: sorted([(i, round(Counter(x)[i] / float(len(x)) * 100.0, 3)) for i in Counter(x)],
+                                   key=lambda x: x[1])
+
+    data_size = lambda x: round(len(x) / len(data), 3)
+
+    logger.info(60 * '*')
+
+    logger.info('Train Dataset real Size: {}'.format(round(len(x_train) / len(data), 3)))
+
+    logger.info('Validation Dataset Requested Size: {}'.format(validation_size))
+    logger.info('Validation Dataset Real Size: {}'.format(data_size(x_val)))
+
+    logger.info('Train Dataset shape: {}'.format(x_train.shape))
+    logger.info('Train Dataset Label Proportion: ')
+    for t in proportions(y_train):
+        logger.info(t)
+
+    logger.info('Validation Dataset shape: {}'.format(x_val.shape))
+    logger.info('Validation Dataset Label Proportion: ')
+    for t in proportions(y_val):
+        logger.info(t)
+
+    df_train = x_train.copy()
+    df_train[label] = y_train
+
+    df_validation = x_val.copy()
+    df_validation[label] = y_val
+
+    result = {
+        'x_train': x_train,
+        'x_validation': x_val,
+        'y_train': y_train,
+        'y_validation': y_val,
+        'df_train': df_train,
+        'df_validation': df_validation,
+    }
+
+    return result
+
+
 if __name__ == "__main__":
 
     mydata = parse_reviews(load_data=True, save_data=True)
-    print(mydata)
+    print(mydata.head())
+
+    splits = get_df_stratified_split_in_train_validation(data=mydata,
+                                                         label='polarity',
+                                                         validation_size=.20)
+
+    print(splits.keys())
