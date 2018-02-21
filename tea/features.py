2dc469a2566bad142ca6712aff5e0f0b75efb849
Perakis Georgios <perakisgeorgios@gmail.com>
2018-02-21 09:52:32 +0200

    implemented SingleColumnDimensionReshaper(). Also added loggers

--- tea/features.py
+++ tea/features.py
@@ -1,8 +1,8 @@
-from AnalyticsEngine import setup_logger
+from tea import setup_logger
 from sklearn.base import BaseEstimator, TransformerMixin
 import pandas as pd
 import numpy as np
-from AnalyticsEngine.text_mining import tokenize_text
+from tea.text_mining import tokenize_text
 
 logger = setup_logger(__name__)
 
@@ -57,6 +57,24 @@ class DenseTransformer(TransformerMixin):
         return self
 
 
+class SingleColumnDimensionReshaper(BaseEstimator, TransformerMixin):
+
+    def __init__(self):
+        """
+
+        :param columns:
+        """
+        pass
+
+    def transform(self, X, y=None):
+        return X.values.reshape(-1, 1)
+
+    def fit(self, X, y=None):
+        """Returns `self` unless something different happens in train and test"""
+
+        return self
+
+
 class WordLengthMetricsExtractor(BaseEstimator, TransformerMixin):
     """Takes in dataframe, extracts text column, splits text in tokens and outputs average word length"""
 
@@ -91,6 +109,7 @@ class WordLengthMetricsExtractor(BaseEstimator, TransformerMixin):
 
     def transform(self, X, y=None):
 
+        logger.info('Calculating {} for "{}" Column'.format(self.metric, self.col_name))
         x = X[self.col_name].apply(lambda s: tokenize_text(text=s, split_type=self.split_type))
 
         return x.apply(self.calculate_metric)
@@ -111,6 +130,7 @@ class TextLengthExtractor(BaseEstimator, TransformerMixin):
         self.col_name = col_name
 
     def transform(self, X, y=None):
+        logger.info('Calculating text length for "{}" Column'.format(self.col_name))
         return X[self.col_name].apply(len)
 
     def fit(self, X, y=None):
@@ -128,6 +148,7 @@ class ContainsSpecialCharactersExtractor(BaseEstimator, TransformerMixin):
         self.SPECIAL_CHARACTERS = set("!@#$%^&*()_+-=")
 
     def transform(self, X, y=None):
+        logger.info('Checking whether text contains special characters for "{}" Column'.format(self.col_name))
 
         return X[self.col_name].apply(lambda s: bool(set(s) & self.SPECIAL_CHARACTERS))
 
@@ -146,10 +167,9 @@ class NumberOfTokensCalculator(BaseEstimator, TransformerMixin):
         self.col_name = col_name
 
     def transform(self, X, y=None):
+        logger.info('Counting number of tokens for "{}" Column'.format(self.col_name))
         return X[self.col_name].apply(lambda x: len(tokenize_text(x, split_type='thorough')))
 
     def fit(self, X, y=None):
         """Returns `self` unless something different happens in train and test"""
         return self
-
-
