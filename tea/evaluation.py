581b7e977aa72651d8990526425da98b7892e5cc
Perakis Georgios <perakisgeorgios@gmail.com>
2018-03-01 16:50:44 +0200

    created benchmark() and create_benchmark_plot() functions for model evaluation

--- tea/evaluation.py
+++ tea/evaluation.py
@@ -1,5 +1,13 @@
 from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, roc_auc_score
 import pandas as pd
+from sklearn import metrics, svm
+import matplotlib.pylab as plt
+from matplotlib.font_manager import FontProperties
+from sklearn import datasets
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+
+plt.rcParams['figure.figsize'] = (16, 8)
 
 
 def create_confusion_matrix(actual, predicted, category):
@@ -75,12 +83,186 @@ def create_clf_report(y_true, y_pred, classes):
     return confusion
 
 
-if __name__ == "__main__":
+def benchmark(clf, train_X, train_y, test_X, test_y):
+    """
+    This function calculates metrics for evaluation of a classifier over a training and a test set.
 
-    a = ['positive','positive','positive','positive','positive','positive','positive',
-         'negative','negative','negative','negative','negative','negative','negative']
+    :param clf: obj. An sklearn classifier
+    :param train_X: array
+    :param train_y: array
+    :param test_X: array
+    :param test_y: array
+    :return: dict
+    """
+    clf.fit(train_X, train_y)
+    pred = clf.predict(test_X)
+
+    f1 = metrics.f1_score(test_y, pred, average='weighted')
+
+    accuracy = metrics.accuracy_score(test_y, pred)
+
+    print(" Acc: %f " % accuracy)
+
+    result = {'f1': f1, 'accuracy': accuracy, 'train size': len(train_y),
+              'test size': len(test_y), 'predictions': pred}
+
+    return result
+
+
+def create_benchmark_plot(train_X,
+                          train_y,
+                          test_X,
+                          test_y,
+                          clf,
+                          splits=20,
+                          plot_outfile=None,
+                          y_ticks=0.025,
+                          min_y_lim=0.4):
+    """
+    This method creates a benchmark plot.
+
+    :param train_X:
+    :param train_y:
+    :param test_X:
+    :param test_y:
+    :param clf:
+    :param splits:
+    :param plot_outfile:
+    :param y_ticks:
+    :param min_y_lim:
+    :return:
+    """
+
+    results = {'train_size': [],
+               'on_test': [],
+               'on_train': []}
+
+    # splitting the train X in n (almost) equal splits)
+    train_x_splits = np.array_split(ary=train_X, indices_or_sections=splits, axis=0)
+
+    # splitting the train y in the same splits as the train X
+    train_y_splits = np.array_split(ary=train_y, indices_or_sections=splits, axis=0)
+
+    # setting parameters for the graph.
+    font_p = FontProperties()
+
+    font_p.set_size('small')
+
+    fig = plt.figure()
+    fig.suptitle('Learning Curves', fontsize=20)
+
+    ax = fig.add_subplot(111)
+    ax.axis(xmin=0, xmax=train_X.shape[0] * 1.05, ymin=0, ymax=1.1)
+
+    plt.xlabel('N. of training instances', fontsize=18)
+    plt.ylabel('Accuracy', fontsize=16)
+
+    plt.grid(True)
+
+    plt.axvline(x=int(train_X.shape[0] * 0.3))
+    plt.yticks(np.arange(0, 1.025, 0.025))
+
+    if y_ticks == 0.05:
+        plt.yticks(np.arange(0, 1.025, 0.05))
+
+    elif y_ticks == 0.025:
+        plt.yticks(np.arange(0, 1.025, 0.025))
+
+    plt.ylim([min_y_lim, 1.025])
+
+    # each time adds up one split and refits the model.
+    for i in range(1, splits + 1):
+        train_x_part = np.concatenate(train_x_splits[:i])
+        train_y_part = np.concatenate(train_y_splits[:i])
+
+        print(20 * '*')
+        print('Split {} size: {}'.format(i, train_x_part.shape))
+
+        results['train_size'].append(train_x_part.shape[0])
+
+        result_on_test = benchmark(clf=clf,
+                                   train_X=train_x_part,
+                                   train_y=train_y_part,
+                                   test_X=test_X,
+                                   test_y=test_y)
+
+        # calculates each time the metrics also on the test.
+        results['on_test'].append(result_on_test['accuracy'])
+
+        # calculates the metrics for the given training part
+        result_on_train_part = benchmark(clf=clf,
+                                         train_X=train_x_part,
+                                         train_y=train_y_part,
+                                         test_X=train_x_part,
+                                         test_y=train_y_part)
+
+        results['on_train'].append(result_on_train_part['accuracy'])
+
+        line_up, = ax.plot(results['train_size'],
+                           results['on_train'],
+                           'o-',
+                           label='Accuracy on Train')
+
+        line_down, = ax.plot(results['train_size'],
+                             results['on_test'],
+                             'o-',
+                             label='Accuracy on Test')
+
+        plt.legend([line_up, line_down],
+                   ['Accuracy on Train', 'Accuracy on Test'],
+                   prop=font_p)
+
+    if plot_outfile:
+        fig.savefig(plot_outfile)
+
+    plt.show()
+
+    return results
+
+
+if __name__ == "__main__":
+    a = ['positive', 'positive', 'positive', 'positive', 'positive', 'positive', 'positive',
+         'negative', 'negative', 'negative', 'negative', 'negative', 'negative', 'negative']
 
     b = ['negative', 'positive', 'negative', 'positive', 'negative', 'positive', 'positive',
          'positive', 'negative', 'negative', 'negative', 'negative', 'negative', 'positive']
 
-    x= create_clf_report(y_true=a, y_pred=b, classes=['positive', 'negative'])
+    create_clf_report(y_true=a, y_pred=b, classes=['positive', 'negative'])
+
+    # Another Example
+
+    digits = datasets.load_digits()
+    X = digits.data
+    Y = digits.target
+
+    # split 80% train , 20% test
+    digits_train_x = X[0: int(X.shape[0] * 0.8), :]
+    digits_train_y = Y[0: int(X.shape[0] * 0.8)]
+    digits_test_x = X[int(X.shape[0] * 0.8):, :]
+    digits_test_y = Y[int(X.shape[0] * 0.8):]
+
+    # normalize values between -1 and 1 with the simple minmax algorithm
+    normalizer = MinMaxScaler(feature_range=(-1.0, 1.0))
+    digits_train_x = normalizer.fit_transform(digits_train_x)
+    digits_test_x = normalizer.transform(digits_test_x)
+
+    from sklearn.utils import shuffle
+
+    train_x_shuffled, train_y_shuffled = shuffle(digits_train_x,
+                                                 digits_train_y,
+                                                 random_state=1989)
+
+    clf_svm = svm.LinearSVC(random_state=1989,
+                            C=100.,
+                            penalty='l2',
+                            max_iter=1000)
+
+    create_benchmark_plot(train_x_shuffled,
+                          train_y_shuffled,
+                          digits_test_x,
+                          digits_test_y,
+                          clf_svm,
+                          10,
+                          None,
+                          0.025,
+                          0.5)
