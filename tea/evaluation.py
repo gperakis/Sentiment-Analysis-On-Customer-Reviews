6b41fea101ed18ae6eaa99c8c493400c92f28830
Marion <marianikol@gmail.com>
2018-03-04 16:53:53 +0200

    Added functions for creating precision-recall curves for multiclass problems.

--- tea/evaluation.py
+++ tea/evaluation.py
@@ -1,11 +1,13 @@
-from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, roc_auc_score
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, roc_auc_score, precision_recall_curve, average_precision_score
 import pandas as pd
-from sklearn import metrics, svm
+from sklearn import metrics, svm, datasets
 import matplotlib.pylab as plt
 from matplotlib.font_manager import FontProperties
-from sklearn import datasets
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, label_binarize
+from sklearn.model_selection import train_test_split
 import numpy as np
+from itertools import cycle
+import seaborn as sns
 
 plt.rcParams['figure.figsize'] = (16, 8)
 
@@ -220,6 +222,110 @@ def create_benchmark_plot(train_X,
     return results
 
 
+def prec_recall_multi(n_classes, X_test, y_test, fitedclf):
+    """
+
+    :param n_classes: int. the number of classes of the data
+    :param X_test: list
+    :param y_test: list
+    :param fitedclf: fitted classifier
+    :return: 3 dictionaries for precision recall, average_precision
+    """
+    y_score = fitedclf.decision_function(X_test)
+    precision = dict()
+    recall = dict()
+    average_precision = dict()
+    for i in range(n_classes):
+        precision[i], recall[i], _ = precision_recall_curve(Y_test[:, i],
+                                                            y_score[:, i])
+        average_precision[i] = average_precision_score(Y_test[:, i], y_score[:, i])
+
+    # A "micro-average": quantifying score on all classes jointly
+    precision["micro"], recall["micro"], _ = precision_recall_curve(Y_test.ravel(),
+                                                                    y_score.ravel())
+    average_precision["micro"] = average_precision_score(Y_test, y_score,
+                                                         average="micro")
+    return precision, recall, average_precision
+
+def plot_micro_prec_recall(precision, recall, average_precision):
+    """
+    Created an average precision plot, micro-averaged over all classes
+    :param precision: dict.
+    :param recall: dict.
+    :param average_precision: dict.
+    :return: plot
+    """
+    sns.set()
+    sns.set_style("dark")
+    plt.figure()
+    plt.step(recall['micro'], precision['micro'], color='b', alpha=0.2,
+             where='post')
+    plt.fill_between(recall["micro"], precision["micro"], step='post', alpha=0.2,
+                     color='b')
+
+    plt.xlabel('Recall', fontsize=18)
+    plt.ylabel('Precision', fontsize=18)
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title(
+        'Average precision score, micro-averaged over all classes: AP={0:0.2f}'
+        .format(average_precision["micro"]), fontsize=24)
+
+    plt.show()
+
+
+def plot_micro_prec_recall_per_class(n_classes, precision, recall, average_precision):
+    """
+
+    :param n_classes: number of classes
+    :param precision: dict.
+    :param recall: dict.
+    :param average_precision: dict.
+    :return: plot
+    """
+
+
+    sns.set()
+    sns.set_style("dark")
+
+    pal = sns.color_palette("cubehelix", 5)
+    colors = cycle(pal.as_hex())
+
+    plt.figure(figsize=(14, 10))
+    f_scores = np.linspace(0.2, 0.8, num=4)
+    lines = []
+    labels = []
+
+    for f_score in f_scores:
+        x = np.linspace(0.01, 1)
+        y = f_score * x / (2 * x - f_score)
+        l, = plt.plot(x[y >= 0], y[y >= 0], color='gray', alpha=0.2)
+        plt.annotate('f1={0:0.1f}'.format(f_score), xy=(0.9, y[45] + 0.02))
+
+    lines.append(l)
+    labels.append('iso-f1 curves')
+    l, = plt.plot(recall["micro"], precision["micro"], color='gold', lw=2)
+    lines.append(l)
+    labels.append('micro-average Precision-recall (area = {0:0.2f})'
+                  ''.format(average_precision["micro"]))
+
+    for i, color in zip(range(n_classes), colors):
+        l, = plt.plot(recall[i], precision[i], color=color, lw=2)
+        lines.append(l)
+        labels.append('Precision-recall for class {0} (area = {1:0.2f})'
+                      ''.format(i, average_precision[i]))
+
+    fig = plt.gcf()
+    fig.subplots_adjust(bottom=0.25)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('Recall', fontsize=18)
+    plt.ylabel('Precision', fontsize=18)
+    plt.title('Extension of Precision-Recall curve to multi-class', fontsize=24)
+    plt.legend(lines, labels, loc=(0, -.38), prop=dict(size=14))
+
+    plt.show()
+
 if __name__ == "__main__":
     a = ['positive', 'positive', 'positive', 'positive', 'positive', 'positive', 'positive',
          'negative', 'negative', 'negative', 'negative', 'negative', 'negative', 'negative']
@@ -266,3 +372,36 @@ if __name__ == "__main__":
                           None,
                           0.025,
                           0.5)
+
+    # Another example for plotting Precision-recall curves for multiclass
+
+    iris = datasets.load_iris()
+    X = iris.data
+    y = iris.target
+    # Add noisy features
+    random_state = np.random.RandomState(0)
+    n_samples, n_features = X.shape
+    X = np.c_[X, random_state.randn(n_samples, 200 * n_features)]
+    # Use label_binarize to be multi-label like settings
+    Y = label_binarize(y, classes=[0, 1, 2])
+    n_classes = Y.shape[1]
+    # Split into training and test
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=.5,
+                                                        random_state=random_state)
+
+    # We use OneVsRestClassifier for multi-label prediction
+    from sklearn.multiclass import OneVsRestClassifier
+
+    # Run classifier
+    classifier = OneVsRestClassifier(svm.LinearSVC(random_state=random_state))
+    classifier.fit(X_train, Y_train)
+
+    #Now plotting begins
+
+    prec, recall, av_prec = prec_recall_multi(n_classes, X_test, Y_test, classifier)
+    print('Average precision score, micro-averaged over all classes: {0:0.2f}'
+          .format(av_prec["micro"]))
+
+    plot_micro_prec_recall(prec, recall, av_prec)
+
+    plot_micro_prec_recall_per_class(n_classes, prec, recall, av_prec)
